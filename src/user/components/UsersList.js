645c463e1bf221947e46f03f0b2f44b5aa997b67
rashedxyz <rashed.webdev@gmail.com>
2022-06-21 23:04:17 +0600

    working on the user related ui components

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./UsersList.css";
+import UserItem from "./UserItem";
+
+const UsersList = (props) => {
+  if (props.items.length === 0) {
+    return <h2>No users found</h2>;
+  }
+  return (
+    <ul>
+      {props.items.map((user) => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,11 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return (
-    <div>
-      <p>Users works!</p>
-    </div>
-  );
+  const USERS = [
+    {
+      id: "u1",
+      name: "John Doe",
+      image:
+        "https://www.fakepersongenerator.com/Face/male/male2017108405017182.jpg",
+      places: 3
+    }
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
